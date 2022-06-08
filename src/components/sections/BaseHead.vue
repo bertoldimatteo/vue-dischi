32f7bc8e63bb1bdfd8b68c4363a5c36248411b4f
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-08 14:55:28 +0200

    aggiunta select header

--- src/components/sections/BaseHead.vue
+++ src/components/sections/BaseHead.vue
@@ -1,6 +1,19 @@
 <template>
     <header>
-      <img src="../../assets/img/spotify-logo.png" alt="logo-spotify">  
+        <div class="header-container">
+            <div class="image">
+                <img src="../../assets/img/spotify-logo.png" alt="logo-spotify"> 
+            </div> 
+            <div class="select-form">
+                <label for="music">Genere:</label>
+                <select id="genere" name="genre-music" form="musicGenre">
+                    <option value="Rock">Rock</option>
+                    <option value="Pop">Pop</option>
+                    <option value="Jazz">Jazz</option>
+                    <option value="Metal">Metal</option>
+                </select>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -13,11 +26,23 @@ export default {
 <style lang="scss" scoped>
 header {
     background-color: var( --primary-color);
+    height: 66px;
     max-height: 66px;
 
     & img {
-        max-width: 4%;
+        max-width: 9%;
         padding: 10px;
     }
+}
+.header-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.select-form {
+    color: #fff;
+}
+select {
+    margin: 10px;
 }
 </style>
